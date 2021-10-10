dfa8255fabeb244e0cebced03f9e06d5c80a49d9
SponzaPlusPlus <61894715+SponzaPlusPlus@users.noreply.github.com>
2021-10-10 17:35:19 +1100

    first enum test

--- proj-budget_gas_project_00/PBMainGTSModule.ts
+++ proj-budget_gas_project_00/PBMainGTSModule.ts
@@ -13,17 +13,33 @@
  * @brief (?)
  */
 
- enum TaxBracket
- {
-    TB_ZERO = 1,
-    TB_ONE,
-    tb_TWO,
-    TB_THREE,
-  }
+enum ExpensePriorityState
+{
+	EPS_UNASSIGNED = 0,
+	EPS_ESSENTIAL = 1,
+	EPS_LUXURY = 2,
+	EPS_UNKNOWN = 3,
+	EPS_NULL = 4,
+	EPS_UNCATEGORISED = 5,
+	EPS_MULTI_CATEGORY = 6,
+	EPS_ALL_CATEGORIES = 7,
+	EPS_NOT_AVAILABLE = 8,
+	EPS_NOT_APPLICABLE = 9,
+	EPS_ERROR = 10,
+	EPS_TEST = 11,
+}
+
+enum TaxBracket
+{
+	TB_ZERO = 1,
+	TB_ONE,
+	TB_TWO,
+	TB_THREE,
+}
 
 const greeter = (person: string) =>
 {
-    return `Hello, ${person}!`;
+	return `Hello, ${person}!`;
 }
   
 let user = 'Grant';
